ca5b9844cd494754e853861972c316a796a84fbf
Kaio Felps <kaiofelipe2005@gmail.com>
2026-06-26 16:04:01 -0300

    style: refator document card as per penpot design

--- lib/prontuario.dart
+++ lib/prontuario.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:front_pi/widgets/document_card.dart';
 import 'package:front_pi/widgets/mainAppBar.dart';
 import 'package:go_router/go_router.dart';
-import 'package:intl/intl.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:front_pi/models/prontuario_document.dart';
 import 'package:front_pi/services/prontuario_service.dart';
-
-final _dateFmt = DateFormat('dd/MM/yyyy');
 
 class ProntuarioPage extends StatefulWidget {
   final String patientId;
@@ -204,106 +202,8 @@ class _ProntuarioPageState extends State<ProntuarioPage> {
             );
           }
 
-          return _DocumentoCard(documento: _documents[index]);
+          return DocumentoCard(documento: _documents[index]);
         },
-      ),
-    );
-  }
-}
-
-// O seu card corrigido fica aqui embaixo, fora da classe da página:
-class _DocumentoCard extends StatelessWidget {
-  final ProntuarioDocument documento;
-
-  const _DocumentoCard({required this.documento});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 14),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(8),
-        border: Border.all(color: Colors.grey.shade200),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.05),
-            blurRadius: 4,
-            offset: const Offset(0, 2),
-          ),
-        ],
-      ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            width: 48,
-            height: 56,
-            decoration: BoxDecoration(
-              color: Colors.grey.shade100,
-              borderRadius: BorderRadius.circular(6),
-            ),
-            child: Center(
-              child: PhosphorIcon(
-                PhosphorIconsFill.fileText,
-                color: Colors.grey.shade500,
-                size: 28,
-              ),
-            ),
-          ),
-          const SizedBox(width: 12),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  documento.title,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 14,
-                    height: 1.1,
-                  ),
-                ),
-                if (documento.description != null &&
-                    documento.description!.isNotEmpty) ...[
-                  const SizedBox(height: 4),
-                  Text(
-                    documento.description!,
-                    style: TextStyle(fontSize: 13, color: Colors.grey.shade700),
-                  ),
-                ],
-                const SizedBox(height: 4),
-                Text(
-                  'Adicionado em ${_dateFmt.format(documento.createdAt)}. '
-                  'Última modificação em ${_dateFmt.format(documento.lastUpdatedAt)}.',
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.grey.shade500,
-                    height: 1.2,
-                  ),
-                ),
-                const SizedBox(height: 6),
-                GestureDetector(
-                  onTap: () {
-                    // TODO: abrir documento via documento.documentUrl
-                  },
-                  child: const Text(
-                    'Baixar o documento',
-                    style: TextStyle(
-                      fontSize: 13,
-                      color: Color(0xFFE6A800),
-                      fontWeight: FontWeight.w600,
-                      decoration: TextDecoration.underline,
-                      decorationColor: Color(0xFFE6A800),
-                      height: 1.1,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
       ),
     );
   }
