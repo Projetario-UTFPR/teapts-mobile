eb4a47196f1893121388184e79be05e28c31879d
Kaio Felps <kaiofelipe2005@gmail.com>
2026-06-26 15:25:03 -0300

    feat: extract `ExpandableSection` generic widget

---
+++ lib/components/expandable-section.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+import '../theme/styles.dart';
+import 'package:gap/gap.dart';
+
+class ExpandableSection extends StatefulWidget {
+  final String title;
+  final List<Widget> actions;
+  final List<Widget> children;
+
+  const ExpandableSection({
+    super.key,
+    required this.title,
+    required this.actions,
+    required this.children,
+  });
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ExpandableSectionState();
+  }
+}
+
+class _ExpandableSectionState extends State<ExpandableSection> {
+  bool _isExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      title: Text(widget.title, style: Styles.titlesBold),
+      childrenPadding: EdgeInsets.zero,
+      tilePadding: EdgeInsets.zero,
+
+      onExpansionChanged: (bool expanded) {
+        setState(() {
+          _isExpanded = expanded;
+        });
+      },
+
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+
+        children: [
+          AnimatedRotation(
+            // 45 graus -> 0 graus
+            turns: _isExpanded ? 0.0 : 0.25,
+            duration: const Duration(milliseconds: 200),
+            child: Icon(
+              PhosphorIcons.caretDown(PhosphorIconsStyle.bold),
+              size: 24,
+              color: Styles.widgetBlackCarret,
+            ),
+          ),
+
+          const Gap(16),
+          ...widget.actions,
+        ],
+      ),
+      shape: const Border(),
+
+      children: [
+        const Gap(24),
+        Column(spacing: 8, children: widget.children),
+      ],
+    );
+  }
+}
--- lib/widgets/expandableEquipeMultidiciplinar.dart
+++ lib/widgets/expandableEquipeMultidiciplinar.dart
@@ -1,77 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:front_pi/components/expandable-section.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 import '../theme/styles.dart';
 import 'imageSideCard.dart';
-import 'package:gap/gap.dart';
 
-class ExpandableEquipeMultidiciplinar extends StatelessWidget {
-  const ExpandableEquipeMultidiciplinar({super.key});
-
+class _ExpandableEquipeMultidisciplinarState
+    extends State<ExpandableEquipeMultidisciplinar> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.zero,
-      color: Colors.transparent,
-      shadowColor: Colors.transparent,
-      elevation: 0,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-      child: ExpansionTile(
-        title: const Text('Equipe Multidisciplinar', style: Styles.titlesBold),
-        childrenPadding: EdgeInsets.zero,
-        tilePadding: EdgeInsets.zero,
-        trailing: Row(
-          mainAxisSize: MainAxisSize.min,
-
-          children: [
-            Icon(
-              PhosphorIcons.caretDown(PhosphorIconsStyle.bold),
-              size: 24,
-              color: Styles.widgetBlackCarret,
-            ),
-
-            const Gap(16),
-            IconButton(
-              icon: Icon(
-                PhosphorIcons.pencilSimpleLine(PhosphorIconsStyle.bold),
-                size: 24,
-                color: Styles.widgetBlack,
-              ),
-              onPressed: () {
-                //TODO: mandar para pagina para editar o conteudo de situação social
-              },
-              style: IconButton.styleFrom(
-                backgroundColor: Styles.widgetYellow,
-                padding: const EdgeInsets.all(0),
-                shape: CircleBorder(),
-              ),
-            ),
-          ],
+    return ExpandableSection(
+      title: "Equipe multidisciplinar",
+      actions: [
+        IconButton(
+          icon: Icon(
+            PhosphorIcons.pencilSimpleLine(PhosphorIconsStyle.bold),
+            size: 24,
+            color: Styles.widgetBlack,
+          ),
+          onPressed: () {
+            //TODO: mandar para pagina para editar o conteudo de situação social
+          },
+          style: IconButton.styleFrom(
+            backgroundColor: Styles.widgetYellow,
+            padding: const EdgeInsets.all(0),
+            shape: CircleBorder(),
+          ),
         ),
-        shape: const Border(),
+      ],
+      children: [
+        CustomRowItem(
+          title: 'Jhon Doe',
+          subtitle: 'Pscicólogo',
+          isCircularImage: true,
+          placeholderIcon: Icons.person,
+        ),
 
-        children: [
-          const Gap(24),
-          CustomRowItem(
-            title: 'Jhon Doe',
-            subtitle: 'Pscicólogo',
-            isCircularImage: true,
-            placeholderIcon: Icons.person,
-            onLinkTap: () => print('Tapped Beta'),
-          ),
-
-          const Gap(8),
-
-          CustomRowItem(
-            title: 'Jhon Doe',
-            subtitle: 'Pscicólogo',
-            isCircularImage: true,
-            placeholderIcon: Icons.person,
-            onLinkTap: () => print('Tapped Beta'),
-          ),
-
-          const Gap(8),
-        ],
-      ),
+        CustomRowItem(
+          title: 'Jhon Doe',
+          subtitle: 'Pscicólogo',
+          isCircularImage: true,
+          placeholderIcon: Icons.person,
+        ),
+      ],
     );
+  }
+}
+
+class ExpandableEquipeMultidisciplinar extends StatefulWidget {
+  const ExpandableEquipeMultidisciplinar({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ExpandableEquipeMultidisciplinarState();
   }
 }
